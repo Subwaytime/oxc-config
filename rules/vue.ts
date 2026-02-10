2c4e14255b846c2d383608fab67e0c9a62b4a607
Subwaytime <leon.l@nophase.de>
2026-02-11 00:32:12 +0100

    add vue rules

---
+++ rules/vue.ts
@@ -0,0 +1,21 @@
+import { defineRules } from "../utils";
+
+export const VUE_RULES = defineRules("vue", {
+    "vue/define-emits-declaration": ["error", "type-literal"],
+    "vue/define-props-declaration": ["error", "type-based"],
+    "vue/define-props-destructuring": ["error", { "destructure": "always" }],
+    "vue/max-props": ["error",  { "maxProps": 10} ],
+    "vue/no-arrow-functions-in-watch": ["off"],
+    "vue/no-deprecated-destroyed-lifecycle": "error",
+    "vue/no-export-in-script-setup": "error",
+    "vue/no-import-compiler-macros": "error",
+    "vue/no-lifecycle-after-await": "error",
+    "vue/no-multiple-slot-args": "error",
+    "vue/no-required-prop-with-default": "error",
+    "vue/no-this-in-before-route-enter": "error",
+    "vue/prefer-import-from-vue": "error",
+    "vue/require-default-export": ["off"],
+    "vue/require-typed-ref": "error",
+    "vue/valid-define-emits": "error",
+    "vue/valid-define-props": "error"
+});
